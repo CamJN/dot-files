96b9db4b2844a6d0b0fd98226aec18028102ab53
Camden Narzt <admin@silversshadows.com>
2015-05-11 12:38:03 -0600

    add stats to script

--- Library/LaunchAgents/script.sh
+++ Library/LaunchAgents/script.sh
@@ -4,16 +4,18 @@ git checkout ajax-data
 DATABASE_URL=postgres://camdennarzt@localhost:5432/arewesmallyet RACK_ENV=production /Users/camdennarzt/.rbenv/shims/bundle exec rake pipeline:compile
 mv app/public/stylesheets/application-*.css tmp/application.css
 mv app/public/javascripts/application-*.js tmp/application_min.js
+curl -O arewesmallyet.dev/data.json
+curl -O arewesmallyet.dev/stats.json
+mv {data,stats}.json tmp/
 rm -rf app/public
 
 git checkout gh-pages
-/usr/local/bin/psql -c "select json_object_agg(day,data order by day) from records;" -d arewesmallyet -o tmp.json -t
-cat tmp.json | tr -d ' ' > data.json
-rm tmp.json
 mv tmp/application_min.js application_min.js
 mv tmp/application.css application.css
+cat tmp/data.json | tr -d ' ' > data.json
+cat tmp/stats.json | tr -d ' ' > stats.json
 
-git add data.json application.css application_min.js
+git add {data,stats}.json application.css application_min.js
 
 git commit -m 'daily json/version update'
 git push

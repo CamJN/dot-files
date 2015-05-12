437dccefca744c8a7d78bacd1f5f814c4aa2716f
Camden Narzt <admin@silversshadows.com>
2015-05-12 11:52:47 -0600

    update script to latest version

--- Library/LaunchAgents/script.sh
+++ Library/LaunchAgents/script.sh
@@ -6,16 +6,24 @@ mv app/public/stylesheets/application-*.css tmp/application.css
 mv app/public/javascripts/application-*.js tmp/application_min.js
 curl -O arewesmallyet.dev/data.json
 curl -O arewesmallyet.dev/stats.json
+\curl arewesmallyet.dev > tmp/index.html
 mv {data,stats}.json tmp/
 rm -rf app/public
+if git diff-index --quiet HEAD ; then
+  echo 'no change'
+else
+  git add app/assets/javascripts/releases.js
+  git commit -m 'update releases'
+  git push
+fi
 
 git checkout gh-pages
-mv tmp/application_min.js application_min.js
+mv tmp/application_min.js application.js
 mv tmp/application.css application.css
 cat tmp/data.json | tr -d ' ' > data.json
 cat tmp/stats.json | tr -d ' ' > stats.json
-
-git add {data,stats}.json application.css application_min.js
+sed -e 's|/public/assets/[^/]*/||g' tmp/index.html > index.html
+git add {data,stats}.json application.css application_min.js index.html
 
 git commit -m 'daily json/version update'
 git push

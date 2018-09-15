e30e783724ec633acfb55bd03b042720042e6140
Camden Narzt <c.narzt@me.com>
2018-09-15 10:57:21 -0600

    Bump awscli w/o brew python

--- homebrew/awscli.rb
+++ homebrew/awscli.rb
@@ -4,8 +4,8 @@ class Awscli < Formula
   desc "Official Amazon AWS command-line interface"
   homepage "https://aws.amazon.com/cli/"
   # awscli should only be updated every 10 releases on multiples of 10
-  url "https://github.com/aws/aws-cli/archive/1.15.70.tar.gz"
-  sha256 "6eed92d90bdb952e68ee5aa752936569ed37a6fc3cb5f042d779fba05183db4d"
+  url "https://github.com/aws/aws-cli/archive/1.16.1.tar.gz"
+  sha256 "c3ac5bd4a55bdf4a41264428e82444b3425e00a00a4e5d7945fbab3b3f520e6b"
   head "https://github.com/aws/aws-cli.git", :branch => "develop"
 
 

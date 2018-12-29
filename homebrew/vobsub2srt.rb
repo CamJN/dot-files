e64178536f3470ea93e389f47b411f30fd3e09bb
Camden Narzt <c.narzt@me.com>
2018-12-29 13:32:40 -0700

    Homebrew fixes

--- homebrew/vobsub2srt.rb
+++ homebrew/vobsub2srt.rb
@@ -12,6 +12,12 @@ class Vobsub2srt < Formula
   depends_on 'ffmpeg'
 
   def install
+    needle = 'set(CMAKE_REQUIRED_INCLUDES ${Tesseract_INCLUDE_DIR})'
+    inreplace "CMakeModules/FindTesseract.cmake", needle, needle+"\n"+'set(CMAKE_REQUIRED_FLAGS "-std=c++11")'
+
+    needle = 'set(CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/CMakeModules)'
+    inreplace "CMakeLists.txt", needle, needle+"\n"+'set(CMAKE_CXX_STANDARD 11)'
+
     mkdir "build" do
       system "cmake", "..", *std_cmake_args
       system "make", "install"

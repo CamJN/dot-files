bd0b48265204f2671d7d8717171f59c65ce48d92
Camden Narzt <c.narzt@me.com>
2021-01-13 11:50:56 -0700

    reduce emacs debugging since things are working for now

--- .emacs.d/lisp/tramphelp.el
+++ .emacs.d/lisp/tramphelp.el
@@ -1,9 +1,9 @@
 ;;; tramphelp.el --- Various functions and settings useful for tramp mode
 
 (require 'tramp)
-(setq tramp-debug-buffer t)
-(setq tramp-verbose 9)
-
+;;(setq tramp-debug-buffer t)
+;;(setq tramp-verbose 9)
+;;(with-current-buffer "*scratch*" (insert "(find-file \"/ssh:secur-t.local:/Users/camdennarzt/.bash_profile\")"))
 (require 'docker-tramp)
 
 (provide 'tramphelp)

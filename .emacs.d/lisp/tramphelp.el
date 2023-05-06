ab1c988b51a6c5e1b0cfeb4589b06e944aecbb78
Camden Narzt <c.narzt@me.com>
2023-05-05 23:24:45 -0600

    emacs changes

--- .emacs.d/lisp/tramphelp.el
+++ .emacs.d/lisp/tramphelp.el
@@ -1,8 +1,12 @@
 ;;; tramphelp.el --- Various functions and settings useful for tramp mode
 
 (require 'tramp)
+;;(tramp-cleanup-all-connections)
 ;;(setq tramp-debug-buffer t)
 ;;(setq tramp-verbose 9)
+;;(setq tramp-shell-prompt-pattern "")
+                                        ;(string-match tramp-shell-prompt-pattern "camdennarzt@upstream ~ % ")
+                                        ;(string-match tramp-shell-prompt-pattern "camdennarzt@upstream ~ $ ")
 ;;(with-current-buffer "*scratch*" (insert "(find-file \"/ssh:secur-t.local:/Users/camdennarzt/.bash_profile\")"))
 (require 'docker-tramp)
 

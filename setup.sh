3ccc7128de9456c1ee0f323f03c8654e2e8d052d
Camden Narzt <c.narzt@me.com>
2023-11-09 21:30:00 -0700

    set dns, protect uncommitted changes in setup script

--- setup.sh
+++ setup.sh
@@ -23,6 +23,10 @@ if [ ! -e ~/Developer/Bash ]; then
 fi
 if [ ! -e ~/Developer/Bash/dot-files ]; then
     git clone git@github.com:CamJN/dot-files.git ~/Developer/Bash/dot-files
+else
+    pushd ~/Developer/Bash/dot-files
+    git diff --exit-code || git commit -am 'tmp'
+    popd
 fi
 
 if ! which -s brew ; then
@@ -39,7 +43,9 @@ comm -12 <(brew tap) <(grep -F untap "$HOMEBREW_BUNDLE_FILE" | cut -w -f3 | tr -
 
 (brew bundle check || brew bundle install --verbose) && brew list | grep -e emacs -e postgresql -e dnsmasq -e libpq -e llvm -e transmission-cli -e gnupg | xargs brew pin
 
+if [ -z "${SKIP_DOCTOR-}" ]; then
 brew doctor --list-checks | grep -Fve cask -e check_user_path_2 -e check_user_path_3 -e check_filesystem_case_sensitive -e check_for_unlinked_but_not_keg_only -e check_for_anaconda -e check_for_bitdefender -e check_for_pydistutils_cfg_in_home -e check_deleted_formula | xargs brew doctor
+fi
 
 function link_dotfiles {
     local path="$1"
@@ -102,9 +108,19 @@ fi
 
 rbenv list | xargs -n1 rbenv install -s
 
+if ! ( networksetup -listlocations | grep -Fqe 'Local DNS' ); then
+    POPULATE=populate
+    networksetup -createlocation 'Local DNS' $POPULATE
+fi
+
+if [ "$(networksetup -getcurrentlocation)" != 'Local DNS' ]; then
+    networksetup -switchtolocation 'Local DNS'
+fi
+
+networksetup -listnetworkserviceorder | grep -Ee '^\([0-9]+\)' | grep -Fve 'VPN' | cut -f 2- -d ' ' | xargs -I{} networksetup -setdnsservers "{}" 127.0.0.1
+
 #google-authenticator -t -d
 #.google_authenticator  .passenger-enterprise-download-token
-
 # mkdir -p "$HOME/Pictures/Screenshots/"
 # defaults write com.apple.screencapture location -string "$HOME/Pictures/Screenshots/"
 # defaults write com.apple.screensaver askForPassword -int 1

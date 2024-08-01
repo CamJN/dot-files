d02f04998b163a21361e47d8a52942bebcbe6a6b
Camden Narzt <c.narzt@me.com>
2024-08-01 10:26:45 -0600

    fixup emacs config

--- .emacs.d/lisp/tramphelp.el
+++ .emacs.d/lisp/tramphelp.el
@@ -5,8 +5,8 @@
 ;;(setq tramp-debug-buffer t)
 ;;(setq tramp-verbose 9)
 ;;(setq tramp-shell-prompt-pattern "")
-                                        ;(string-match tramp-shell-prompt-pattern "camdennarzt@upstream ~ % ")
-                                        ;(string-match tramp-shell-prompt-pattern "camdennarzt@upstream ~ $ ")
+;;(string-match tramp-shell-prompt-pattern "camdennarzt@upstream ~ % ")
+;;(string-match tramp-shell-prompt-pattern "camdennarzt@upstream ~ $ ")
 ;;(with-current-buffer "*scratch*" (insert "(find-file \"/ssh:secur-t.local:/Users/camdennarzt/.bash_profile\")"))
 
 (require 'tramp-container)

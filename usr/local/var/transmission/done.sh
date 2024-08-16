a4397116c436ddb8709b3a7f17136bb92e3a5cf0
Camden Narzt <c.narzt@me.com>
2024-08-15 21:15:26 -0600

    fix transmission done script

--- usr/local/var/transmission/done.sh
+++ usr/local/var/transmission/done.sh
@@ -5,14 +5,14 @@
 ###
 
 # TR_APP_VERSION - Transmission's short version string, e.g. 4.0.0
-# TR_TIME_LOCALTIME
+# TR_TIME_LOCALTIME - Current time in this format: Thu Aug 15 21:09:36 2024
 # TR_TORRENT_BYTES_DOWNLOADED - Number of bytes that were downloaded for this torrent
 # TR_TORRENT_DIR - Location of the downloaded data
 # TR_TORRENT_HASH - The torrent's info hash
-# TR_TORRENT_ID
-# TR_TORRENT_LABELS - A comma-delimited list of the torrent's labels
+# TR_TORRENT_ID - Transmission's ID for the torrent
+# TR_TORRENT_LABELS - A comma-delimited list of the torrent's labels, may be empty
 # TR_TORRENT_NAME - Name of torrent (not filename)
 # TR_TORRENT_PRIORITY - The priority of the torrent (Low is "-1", Normal is "0", High is "1")
 # TR_TORRENT_TRACKERS - A comma-delimited list of the torrent's trackers' announce URLs
 
-echo transmission-remote --torrent "$TR_TORRENT_ID" --remove > /tmp/transmission.done.log
+transmission-remote --torrent "$TR_TORRENT_ID" --remove

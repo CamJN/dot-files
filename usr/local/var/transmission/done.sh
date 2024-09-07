da0a5eebcd5fca6e26741307226997a6338b1c5d
Camden Narzt <c.narzt@me.com>
2024-09-06 23:29:55 -0600

    fix transmission done script

--- usr/local/var/transmission/done.sh
+++ usr/local/var/transmission/done.sh
@@ -4,6 +4,12 @@
 ###  ENV vars
 ###
 
+PREFIX="$(/usr/bin/env -P /usr/local/bin:/opt/homebrew/bin brew --prefix)"
+
+exec >> "$PREFIX/var/log/transmission.done.log" 2>&1
+
+PATH="$PREFIX/bin:$PATH"
+
 # TR_APP_VERSION - Transmission's short version string, e.g. 4.0.0
 # TR_TIME_LOCALTIME - Current time in this format: Thu Aug 15 21:09:36 2024
 # TR_TORRENT_BYTES_DOWNLOADED - Number of bytes that were downloaded for this torrent
@@ -15,4 +21,5 @@
 # TR_TORRENT_PRIORITY - The priority of the torrent (Low is "-1", Normal is "0", High is "1")
 # TR_TORRENT_TRACKERS - A comma-delimited list of the torrent's trackers' announce URLs
 
+echo "running \`transmission-remote --torrent $TR_TORRENT_ID --remove\`"
 transmission-remote --torrent "$TR_TORRENT_ID" --remove

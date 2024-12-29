836b2cb943006875f1f0426697fb960246bb35d8
Camden Narzt <c.narzt@me.com>
2024-12-29 14:26:08 -0700

    fix dnsmasq update script

--- usr/local/bin/dnsmasq.update.sh
+++ usr/local/bin/dnsmasq.update.sh
@@ -4,6 +4,6 @@ BLACKLIST=https://raw.githubusercontent.com/notracking/hosts-blocklists/master/d
 
 WHITELIST=(ident.me appboy-images.com icanhazip.com cdn.braze.eu use-application-dns.net responsys.net)
 
-/usr/bin/curl $BLACKLIST | /usr/bin/fgrep -v ${WHITELIST[@]/#/-e } | sed -e '/^#/d' -e 's/address=/server=/' > ./dnsmasq.blacklist.txt.new
+/usr/bin/curl $BLACKLIST | /usr/bin/grep -Fv ${WHITELIST[@]/#/-e } | /usr/bin/sed -e '/^#/d' -e 's/address=/server=/' > ./dnsmasq.blacklist.txt.new
 
-find . -name dnsmasq.blacklist.txt.new \( -size +1M -and -exec cat {} \> ./dnsmasq.blacklist.txt \; -or -true \) -delete
+/usr/bin/find . -name dnsmasq.blacklist.txt.new \( -size +1M -and -exec bash -c '/bin/cat {} > ./dnsmasq.blacklist.txt' \; -or -true \) -delete

f2e7fea087b46eff072d4bb8f627e07835ce8cd6
Camden Narzt <c.narzt@me.com>
2026-03-22 14:44:34 -0600

    deal with tart/lima differences

--- vm-setup.sh
+++ vm-setup.sh
@@ -33,18 +33,21 @@ function update_tcc_database() {
 	EOF
 }
 
+sudo sysctl -w net.inet.tcp.tso=0
+# we add our own value to this so run on tart too
 update_tcc_database "${HOME}/Library/Application Support/com.apple.TCC/TCC.db"
+
+if [ "$(dscl -plist . -read "/Users/$USER" RealName | plutil -extract 'dsAttrTypeStandard:RealName.0' raw -)" != "Managed via Tart" ]; then
+    automationmodetool enable-automationmode-without-authentication
+    defaults -currentHost write com.apple.screensaver idleTime -int 0
+    sudo defaults write /Library/Preferences/com.apple.screensaver loginWindowIdleTime -int 0
+    sudo systemsetup -setsleep Off
+    sysadminctl -screenLock off -password "$(<~/password)"
+fi
 
 # Must be after tcc database
 osascript -e 'tell application "System Events" to tell every desktop to set picture to "/System/Library/Desktop Pictures/Solid Colors/Black.png" as POSIX file'
-
-automationmodetool enable-automationmode-without-authentication
-sudo sysctl -w net.inet.tcp.tso=0
 defaults write com.apple.universalaccess reduceTransparency -bool true
-defaults -currentHost write com.apple.screensaver idleTime -int 0
-sudo defaults write /Library/Preferences/com.apple.screensaver loginWindowIdleTime -int 0
-sudo systemsetup -setsleep Off
-sysadminctl -screenLock off -password "$(cat ~/password)"
 
 export SKIP_CASE_CHECK=true # not setup in vm image
 export SKIP_HOMEBREW_BUNDLE_APPS=true # can't login to mac app store

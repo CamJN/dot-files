5de3dda7dc1925e276bc5b49c335b5d3b9d96048
Camden Narzt <c.narzt@me.com>
2026-04-11 12:17:11 -0600

    make a sensible yank in rectangle mode

--- .emacs.d/lisp/defuns.el
+++ .emacs.d/lisp/defuns.el
@@ -373,6 +373,20 @@ The SEPARATOR regexp defaults to \"\\s-+\"."
    )
   )
 
+;; -- rectangles --
+
+(defun my-rectangle-yank ()
+  "Yank a rectangle as if it was an ordinary kill."
+  (interactive "*")
+  (when (and (use-region-p) (delete-selection-mode))
+    (delete-region (region-beginning) (region-end)))
+  (save-restriction
+    (narrow-to-region (point) (mark))
+    (yank-rectangle)))
+
+(global-set-key (kbd "C-x r C-y") #'my-rectangle-yank)
+
+
 (provide 'defuns)
 
 ;;; defuns.el ends here
